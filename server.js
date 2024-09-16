cd5398aec7f35ca165c3cd16b5ade5be360837ae
Garvish Jain <heygarvish@gmail.com>
2024-09-16 22:57:38 +0530

    done with this.

--- server.js
+++ server.js
@@ -12,21 +12,25 @@ app.use(express.json());
 
 const stripe = require("stripe")(STRIPE_PRIVATE_KEY)
 
-app.get("/create-checkout-session", async (req, res) => {
+app.post("/create-checkout-session", async (req, res) => {
+    const { customer_email } = req.body;
+
     try {
       const session = await stripe.checkout.sessions.create({
-        success_url: 'http://localhost:3000/success',
-        cancel_url: 'http://localhost:3000/cancel',
+        success_url: 'example.com/success',
+        cancel_url: 'example.com/cancel',
+        customer_email: customer_email,
         line_items: [
           {
             price: STRIPE_PRICE_ID,
             quantity: 1,
           },
         ],
+        subscription_data: {
+          trial_period_days: 7,
+        },
         mode: 'subscription',
       });
-
-      console.log("session: ", session.id, session.url, session)
     
       res.json({ url: session.url, session_id: session.id });
     } catch (e) {
@@ -34,13 +38,12 @@ app.get("/create-checkout-session", async (req, res) => {
     }
 })
 
-app.get("/stripe-session", async (req, res) => {
+app.get("/get-stripe-session", async (req, res) => {
     const { stripe_session_id } = req.query;
 
     try {
         // check session
         const session = await stripe.checkout.sessions.retrieve(stripe_session_id);
-        console.log("session: ", session);
   
         // const sessionResult = {
         //   id: 'cs_test_a1lpAti8opdtSIDZQIh9NZ6YhqMMwC0H5wrlwkUEYJc6GXokj2g5WyHkv4',
@@ -54,13 +57,12 @@ app.get("/stripe-session", async (req, res) => {
         // }
         
       
-        // update the user
         res.send({
-            "session_id": session.id,
-            "subscription_id": session.subscription,
-            "customer_id": session.customer,
-            "status": session.status === "complete" ? "success" : "failed"
-        })
+            session_id: session.id,
+            customer_id: session.customer,
+            status: session.status,
+            subscription_id: session.subscription
+        });
     } catch (error) {
         // handle the error
         console.error("an error occurred while retrieving the stripe session:", error);
@@ -70,6 +72,24 @@ app.get("/stripe-session", async (req, res) => {
         });
     }
   })
+
+app.delete("/cancel-subscription", async (req, res) => {
+    const { subscription_id } = req.body;
+
+    try {
+        await stripe.subscriptions.cancel(
+            subscription_id
+          );
+
+        res.status(200).send();
+    } catch (error) {
+        console.error("an error occurred while canceling the subscription:", error);
+
+        return res.status(500).json({
+            message: error
+        });
+    }
+});
 
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`);
